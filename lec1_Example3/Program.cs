51878637f0fff7736707c16f3f1c2f9e2dac4c29
Mikhail Kazachkin <mikhail.kazachkin@yandex.ru>
2023-01-24 21:00:05 +0300

    Example3 created

--- lec1_Example3/Program.cs
+++ lec1_Example3/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 3;
+int numberB = 5;
+int sumResult = numberA + numberB;
+Console.WriteLine("Сумма чисел " + numberA +" и " + numberB + " равна " + sumResult);
