3c11c6d95c508e5394e06a6b0d26515d4b9c9f69
Mikhail Kazachkin <mikhail.kazachkin@yandex.ru>
2023-01-26 20:47:04 +0300

    task 0 completed

---
+++ task00/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 1 на семинаре.
+// Написать программу, считающую квадрат числа
+Console.Clear();
+Console.Write("Введите целое число: ");
+int userNumber = int.Parse(Console.ReadLine ()!)!;
+int result = userNumber * userNumber!;
+Console.WriteLine ("Квадрат числа = " + result);
