703fe78a7771f6189da556d0a5fa32ef8702eed0
Mikhail Kazachkin <mikhail.kazachkin@yandex.ru>
2023-01-26 21:24:55 +0300

    task 01 completed

---
+++ task01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2. Получить от пользователя 2 числа и проверить
+// Является ли одно из них квадратом другого
+Console.Clear();
+Console.Write ("Введите первое целое число: ");
+int firstNumber = int.Parse(Console.ReadLine()!)!;
+Console.Write ("Введите второе целое число: ");
+int secondNumber = int.Parse(Console.ReadLine()!)!;
+int sqrFirst = firstNumber*firstNumber;
+int sqrSecond = secondNumber*secondNumber;
+if (firstNumber==sqrSecond)
+{
+    Console.WriteLine ("Первое число является квадратом второго.");
+} else if (secondNumber == sqrFirst)
+{
+    Console.WriteLine ("Второе число является квадратом первого.");
+} else 
+{
+    Console.WriteLine ("Ни одно из чисел не является квадратом второго.");
+}
