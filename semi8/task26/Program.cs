5503e8c4dd72874c0792941e3870404aeae8e32f
Mikhail Kazachkin <mikhail.kazachkin@yandex.ru>
2023-02-21 21:28:43 +0300

    seminar 8 completed

---
+++ semi8/task26/Program.cs
@@ -0,0 +1,6 @@
+﻿using Geekbrains;
+
+double[,] array = Double2DArray.MakeCustom(true);
+Double2DArray.Print(array, 3, " {0:f0}");
+Console.WriteLine();
+Double2DArray.Print(Double2DArray.FrequencyDictionary(array), 6, " {0:f0}");
